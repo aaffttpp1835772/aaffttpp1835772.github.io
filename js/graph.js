7079ac9fa076f4198a45d6b208daddc78d2fd0f8
aaffttpp1835772 <diops05622@gmail.com>
2023-06-28 06:22:54 +0900

    Update graph.js

--- js/graph.js
+++ js/graph.js
@@ -17,7 +17,7 @@ function updateGraph() {
     }
 
     // Create ticks and labels
-    for(let i = 0; i <= 1; i += 0.1) {
+    for(let i = 0; i <= 1; i += 0.16667) {
         createTick(i);
         createLabel(i);
     }
